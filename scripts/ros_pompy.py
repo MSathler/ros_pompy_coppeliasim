634b58de633a26667fd8bb45a697a1546d889918
MSathler <mauriciossr@hotmail.com>
2020-11-10 20:57:38 -0300

    first commit

---
+++ scripts/ros_pompy.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import numpy as np
+import rospy
+from pompy import models, processors
+from ros_pompy_point_cloud import pompy_point_cloud
+
+if __name__ == '__main__':
+    # Seed random number generator
+    seed = 20180517
+    rng = np.random.RandomState(seed)
+
+    # Define wind model simulation region
+    wind_region = models.Rectangle(x_min=0., x_max=100., y_min=-50., y_max=50.)
+
+    # Define wind model parameters
+    wind_model_params = { 
+        'n_x': 21,
+        'n_y': 21,
+        'u_av': 1.,
+        'v_av': 0.,
+        'k_x': 10.,
+        'k_y': 10.,
+        'noise_gain': 20.,
+        'noise_damp': 0.1,
+        'noise_bandwidth': 0.2,
+        'use_original_noise_updates': True
+    }
+
+    # Create wind model object
+    wind_model = models.WindModel(wind_region, rng=rng, **wind_model_params)
+
+    # Define plume simulation region
+    # This is a subset of the wind simulation region
+    sim_region = models.Rectangle(x_min=0., x_max=50., y_min=-12.5, y_max=12.5)
+
+    # Define plume model parameters
+    plume_model_params = {
+        'source_pos': (0., -12., 0.),
+        'centre_rel_diff_scale': 2.,
+        'puff_release_rate': 10,
+        'puff_init_rad': 0.001**0.5,
+        'puff_spread_rate': 0.001,
+        'init_num_puffs': 10,
+        'max_num_puffs': 10000,
+        'model_z_disp': False,
+    }
+
+    # Create plume model object
+    plume_model = models.PlumeModel(
+        rng=rng, sim_region=sim_region, wind_model=wind_model, **plume_model_params)
+
+    # Define concentration array (image) generator parameters
+    array_gen_params = {
+        'array_z': 0.,
+        'n_x': 500,
+        'n_y': 250,
+        'puff_mol_amount': 8.3e8
+    }
+
+    # Create concentration array generator object
+    array_gen = processors.ConcentrationArrayGenerator(
+        array_xy_region=sim_region, **array_gen_params)
+
+    # Display initial concentration field
+    conc_array = array_gen.generate_single_array(plume_model.puff_array)
+
+    # Simulation timestep
+    dt = 0.01
+
+    # Run wind model forward to equilibrate
+    
+    for k in range(2000):
+    
+        wind_model.update(dt)
+
+    # Iniciate function to ros
+    rospy.init_node('plume_simulator_server')
+
+    try:
+        pb = pompy_point_cloud(wind_model=wind_model,plume_model=plume_model, conc_array = conc_array, array_gen = array_gen)
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        rospy.loginfo('caught exception')
---
+++ scripts/ros_pompy_point_cloud.py
@@ -0,0 +1,134 @@
+import rospy
+from sensor_msgs.msg import PointCloud,ChannelFloat32
+from pompy import models, processors
+import geometry_msgs
+import std_msgs
+from geometry_msgs.msg import Point32, Pose
+
+
+class pompy_point_cloud(object):
+    
+    
+    def __init__(self, wind_model, plume_model, conc_array, array_gen, use_pose_coppelia = True, node_name = 'plume_simulator_server', dt = 0.01, topic_publisher = 'point_pompy',topic_subscriber = '/dummy/pose',frame = 'world'):
+        
+        self._topic_publisher = topic_publisher
+        self._topic_subscriber = topic_subscriber
+        self._frame = frame
+        self._conc_array = conc_array
+        self._wind_model = wind_model
+        self.dt = dt
+        self._node_name = node_name
+        self._array_gen = array_gen
+        self._plume_model = plume_model
+        self._use_pose_coppelia = use_pose_coppelia
+
+        self.ros()
+
+
+    def ros(self):
+
+        rospy.init_node(str(self._node_name))
+        self._pc_publisher = rospy.Publisher(str(self._topic_publisher),PointCloud, queue_size= 100)
+
+        if (self._use_pose_coppelia == True):
+            self._pose_subscriber = rospy.Subscriber(str(self._topic_subscriber),Pose,self.pose_callback)
+            rospy.loginfo("Utilized dummy position with Coppelia Pose")
+        else:
+            rospy.loginfo("Without using the pose with Coppelia")
+            while not rospy.is_shutdown():
+                self.without_coppelia() 
+
+        rospy.spin()
+
+    def pose_callback(self, pose):
+
+        for i in range(10):
+
+            self._wind_model.update(self.dt)
+            self._plume_model.update(self.dt)
+
+        self._conc_array = self._array_gen.generate_single_array(self._plume_model.puff_array)
+        
+        # Initiation of variables temporary variables
+        x =[]
+        y = []
+        z = []
+        inten = []
+
+        # Creation of Point Cloud object
+        self.pc_msg = PointCloud()
+        self.pc_msg.header.stamp = rospy.Time.now()
+        self.pc_msg.header.frame_id = 'world'
+        
+
+        for i in range(self._conc_array.T.shape[0]):
+
+            for j in range(self._conc_array.T.shape[1]):
+
+                if (self._conc_array.T[i][j] != 0):
+
+                    x.append((i*0.01) + pose.position.x)
+                    y.append((j*0.01) + pose.position.y)
+                    z.append(pose.position.z)
+                    inten.append(self._conc_array.T[i][j]*0.001)
+
+
+                    
+        self.pc_msg.points = [Point32(x, y, z) for x, y, z in zip(x, y, z)]
+        self.channel = ChannelFloat32()
+        self.channel.name = 'intensity'
+        self.channel.values = inten
+        self.pc_msg.channels.append(self.channel)
+        
+        # Publshing the Point Cloud
+        self._pc_publisher.publish(self.pc_msg)
+
+        # Reset Variables
+        x = []
+        y = []
+        z = []
+        inten = []
+
+    def without_coppelia(self):
+        for i in range(10):
+
+            self._wind_model.update(self.dt)
+            self._plume_model.update(self.dt)
+
+        self._conc_array = self._array_gen.generate_single_array(self._plume_model.puff_array)
+        
+        # Initiation of variables temporary variables
+        x =[]
+        y = []
+        z = []
+        inten = []
+        # Creation of Point Cloud object
+        self.pc_msg = PointCloud()
+        self.pc_msg.header.stamp = rospy.Time.now()
+        self.pc_msg.header.frame_id = 'world'
+
+        for i in range(self._conc_array.T.shape[0]):
+
+            for j in range(self._conc_array.T.shape[1]):
+
+                if (self._conc_array.T[i][j] != 0):
+
+                    x.append(i*0.01)
+                    y.append(j*0.01)
+                    z.append(0.0)
+                    inten.append(self._conc_array.T[i][j]*0.001)
+
+        self.pc_msg.points = [Point32(x, y, z) for x, y, z in zip(x, y, z)]
+        self.channel = ChannelFloat32()
+        self.channel.name = 'intensity'
+        self.channel.values = inten
+        self.pc_msg.channels.append(self.channel)
+        
+        # Publshing the Point Cloud
+        self._pc_publisher.publish(self.pc_msg)
+
+        # Reset Variables
+        x = []
+        y = []
+        z = []
+        inten = []
